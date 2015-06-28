9c8cd4767a9eddac8621d039da69bc59a91642c9
Jason Miller <aidenn0@geocities.com>
2015-06-28 15:39:33 -0700

    Put each route in its own function

--- src/clackup.lisp
+++ src/clackup.lisp
@@ -40,7 +40,7 @@
 	      (format ,stream-name "var PREPENDPATH = '~a'" *prepend-path*))
 	     (:div :hidden t
 		    :id "csrf"
-		    (esc (session-csrf-token session)))
+		    (esc (session-csrf-token (getf env :session))))
 	      (:div :id "react-content")
 	     ,@body)))))
 
@@ -106,14 +106,201 @@
 
 (defparameter *max-length* (* 1024 1024))
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *app-rules* (make-hash-table)))
+
+(defmacro defapprule (name pattern &body b)
+  (let ((vars (optima::pattern-variables (optima::parse-pattern pattern))))
+    `(progn
+       (defun ,name ,(append '(env body) vars)
+	 (declare (ignorable env body))
+	 ,@b)
+       (setf (gethash ',name *app-rules*) ',pattern))))
+
+(defmacro expand-app-rules (env body &body default-rule)
+  (once-only (env body)
+    `(optima:match ,env
+       ,@(loop for fname being the hash-keys of *app-rules*
+	    using (hash-value pattern)
+	  collect `(,pattern
+		    (,fname ,env ,body
+			    ,@(optima::pattern-variables
+			       (optima::parse-pattern pattern)))))
+       (_ ,@default-rule))))
+
+(defapprule autocomplete (property :path-info (ppcre "^/complete/(.*)" thing))
+  (fccs-complete thing body))
+
+(defapprule lztest (property :path-info "/lztest/")
+  (let ((codes
+	 (map 'list #'char-code
+	      (babel:octets-to-string body :encoding :utf-8))))
+    (log:info "Compressed-Body-Len ~D" (length body))
+    (log:debug codes)
+    (lzstring::decompress body))
+  `(200
+    ()
+    ()))
+
+(defapprule root-rule (property :path-info "/")
+  (with-db-connection
+    `(200
+      (:content-type "text/html")
+      (,(render-page
+	 (s)
+	 (:script
+	  (princ "React.render(React.createElement(CharacterList, {value : " s)
+	  (princ "Immutable.fromJS(" s)
+	  (encode-classish
+	   (loop for id in (get-all-character-ids)
+	      for character = (get-character id)
+	      collect (make-character-summary
+		       :id id
+		       :char-name
+		       (aget :character-name character)
+		       :player-name
+		       (aget :player-name character)
+		       :career-level
+		       (calculate-field :career-level character)))
+	   s)
+	  (princ ")}),document.getElementById(\"react-content\"))" s)))))))
+
+(defapprule new-character-rule (and (property :request-method :POST)
+				    (property :remote-user username)
+				    (property :path-info "/new-character/"))
+  (with-db-connection
+    (let ((id (new-character username)))
+      `(303
+	(:location ,(fixup-path (format nil "/character/~A" id))
+		   :content-type "text/html")
+	(,(cl-who:with-html-output-to-string
+	   (s)
+	   (:htm (:head) (:body "See Other"))))))))
+
+
+(defapprule pdf-rule (property :path-info (ppcre "^/pdf-character/(\\d*)$" id))
+  (let ((character
+	 (with-db-connection
+	   (get-character id))))
+    (when character
+      (let*
+	  ((pdf (fill-pdf character))
+	   (bin-pdf (make-array (length pdf)
+				:element-type '(unsigned-byte 8))))
+	(map-into bin-pdf #'char-code pdf)
+	`(200
+	  (:content-length ,(length bin-pdf)
+			   :content-type "application/pdf")
+	  ,bin-pdf)))))
+
+(defapprule save-char-rule (and (property :request-method :POST)
+				(property :path-info (ppcre "^/save-character/(\\d*)$" id))
+				(property :remote-user username))
+  (log:info "Uncompressed-Body-Len ~D" (length body))
+  (with-db-connection
+    (cond
+      ((null (get-character id))
+       `(404			; No such character
+	 (:content-type "text/html")
+	 (cl-who:with-html-output-to-string (s)
+	   (:htm (:head) (:body "Couldn't locate character")))))
+      ((not (user-can-edit-character-p id username))
+       `(403			;Wrong Permissions
+	 (:content-type "text/html")
+	 (cl-who:with-html-output-to-string (s)
+	   (:htm (:head) (:body "Forbidden")))))
+      (t				; Chaacter exists and We have permisison to edit it
+       (let* ((parsed-body (parse-json-body body))
+	      (fixed-char (fixup-fc-character parsed-body)))
+	 (when (fc-character-p fixed-char)
+	   (log:info "We have a character!")
+	   (save-character id fixed-char))
+	 `(200
+	   (:content-type "text/html")))))))
+
+(defapprule view-char-rule (property :path-info (ppcre "^/view-character/(\\d*)$" id))
+  (with-db-connection
+    (let ((character (get-character id)))
+      (if character
+	  `(200
+	    (:content-type "text/html")
+	    (,(render-page
+	       (s)
+	       (:script
+		(format s (ps:ps (setf *view-only* t)))
+		(format s "React.render(React.createElement(CharacterMenu, {
+characterId: ~D, defaultValue : " id)
+		(princ "fixupFcCharacter(Immutable.fromJS(" s)
+		(encode-classish character s)
+		(princ "))}),document.getElementById(\"react-content\"))" s)))))
+	  `(404
+	    (:content-type "text/html")
+	    (,(cl-who:with-html-output-to-string (s)
+						 (:htm (:head)
+						       (:body (:P "Error: could-not-find character"))))))))))
+
+
+(defapprule edit-char-rule (and
+			    (property :path-info (ppcre "^/character/(\\d*)$" id))
+			    (property :remote-user username))
+  (with-db-connection
+    (if (user-can-edit-character-p id username)
+	(let ((character (get-character id)))
+	  (if character
+	      `(200
+		(:content-type "text/html")
+		(,(render-page
+		   (s)
+		   (:script
+		    (format s "React.render(React.createElement(CharacterMenu, {characterId: ~D, defaultValue : " id)
+		    (princ "fixupFcCharacter(Immutable.fromJS(" s)
+		    (encode-classish character s)
+		    (princ "))}),document.getElementById(\"react-content\"))" s)))))
+	      `(404
+		(:content-type "text/html")
+		(,(cl-who:with-html-output-to-string (s)
+						     (:htm (:head)
+							   (:body (:P "Error: could-not-find character"))))))))
+	`(303
+	  (:location ,(fixup-path (format nil "/view-character/~d" id)))
+	  (())))))
+
+
+(defapprule logout-rule (and
+			 (property :path-info "/logout/")
+			 (property :session session))
+  (invalidate-session session)
+  `(303
+    (:location ,(fixup-path "/"))
+    (())))
+
+(defapprule account-rule (and
+			  (property :path-info "/account/")
+			  (property :remote-user username))
+  (with-db-connection
+    `(200
+      (:content-type "text/html")
+      (,(render-page
+	 (s)
+	 (:script
+	  (format s
+		  "React.render(React.createElement(AccountSettings, {user: \"~A\"}),document.getElementById(\"react-content\"))" username)))))))
+
+(defapprule set-password-rule (and
+			       (property :path-info "/set-password/")
+			       (property :request-method :post)
+			       (property :remote-user username))
+  (change-user-password username
+			(flexi-streams:octets-to-string body :external-format :utf-8))
+  `(200
+    (:content-type "text/html")))
+
 (defun app (env)
   (log4cl:log-info env)
   (log4cl:log-info "Headers: ~S" (alexandria:hash-table-plist (getf env :headers)))
   (setf (getf env :path-info)
 	(strip-path-prefix (getf env :path-info)))
-  (let ((content-length (getf env :content-length))
-	(body)
-	(session (getf env :session)))
+  (let ((content-length (getf env :content-length)) (body))
     (log:info content-length)
     (cond
       ((null content-length))
@@ -130,167 +317,11 @@
 	  (:content-type "text/html")
 	  (,(with-html-output-to-string (s)
 					(:html (:body (:p "Error"))))))
-      (match env
-	((property :path-info (ppcre "^/complete/(.*)" thing))
-	 (fccs-complete thing body))
-	((property :path-info "/lztest/")
-	 (let ((codes
-		(map 'list #'char-code
-		     (babel:octets-to-string body :encoding :utf-8))))
-	   (log:info "Compressed-Body-Len ~D" (length body))
-	   (log:debug codes)
-	   (lzstring::decompress body
-	    #+(or)(make-array (length codes) :element-type '(unsigned-byte 16)
-			:initial-contents codes)))
-	 `(200
-	   ()
-	   ()))
-	((property :path-info "/")
-	 (with-db-connection
-	 `(200
-	   (:content-type "text/html")
-	     (,(render-page
-	      (s)
-	      (:script
-	       (princ "React.render(React.createElement(CharacterList, {value : " s)
-	       (princ "Immutable.fromJS(" s)
-	       (encode-classish
-		(loop for id in (get-all-character-ids)
-		   for character = (get-character id)
-		   collect (make-character-summary
-			    :id id
-			    :char-name
-			    (aget :character-name character)
-			    :player-name
-			    (aget :player-name character)
-			    :career-level
-			    (calculate-field :career-level character)))
-		s)
-	       (princ ")}),document.getElementById(\"react-content\"))" s)))))))
-	((and (property :request-method :POST)
-	      (property :remote-user username)
-	      (property :path-info "/new-character/"))
-	 (with-db-connection
-	   (let ((id (new-character username)))
-	     `(303
-	       (:location ,(fixup-path (format nil "/character/~A" id))
-			  :content-type "text/html")
-	       (,(cl-who:with-html-output-to-string
-		  (s)
-		  (:htm (:head) (:body "See Other"))))))))
-	((property :path-info (ppcre "^/pdf-character/(\\d*)$" id))
-	 (let ((character
-		(with-db-connection
-		  (get-character id))))
-	   (when character
-	     (let*
-		 ((pdf (fill-pdf character))
-		  (bin-pdf (make-array (length pdf)
-				       :element-type '(unsigned-byte 8))))
-	       (map-into bin-pdf #'char-code pdf)
-	       `(200
-		 (:content-length ,(length bin-pdf)
-				  :content-type "application/pdf")
-		 ,bin-pdf)))))
-	((and (property :request-method :POST)
-	      (property :path-info (ppcre "^/save-character/(\\d*)$" id))
-	      (property :remote-user username))
-	 (log:info "Uncompressed-Body-Len ~D" (length body))
-	 (with-db-connection
-	   (cond
-	     ((null (get-character id))
-	      `(404			; No such character
-		(:content-type "text/html")
-		(cl-who:with-html-output-to-string (s)
-		  (:htm (:head) (:body "Couldn't locate character")))))
-	     ((not (user-can-edit-character-p id username))
-	      `(403			;Wrong Permissions
-		(:content-type "text/html")
-		(cl-who:with-html-output-to-string (s)
-		  (:htm (:head) (:body "Forbidden")))))
-	     (t				; Chaacter exists and We have permisison to edit it
-	      (let* ((parsed-body (parse-json-body body))
-		     (fixed-char (fixup-fc-character parsed-body)))
-		(when (fc-character-p fixed-char)
-		  (log:info "We have a character!")
-		  (save-character id fixed-char))
-		`(200
-		  (:content-type "text/html")))))))
-	((property :path-info (ppcre "^/view-character/(\\d*)$" id))
-	 (with-db-connection
-	   (let ((character (get-character id)))
-	     (if character
-		 `(200
-		   (:content-type "text/html")
-		   (,(render-page
-		      (s)
-		      (:script
-		       (format s (ps:ps (setf *view-only* t)))
-		       (format s "React.render(React.createElement(CharacterMenu, {
-characterId: ~D, defaultValue : " id)
-		       (princ "fixupFcCharacter(Immutable.fromJS(" s)
-		       (encode-classish character s)
-		       (princ "))}),document.getElementById(\"react-content\"))" s)))))
-		 `(404
-		   (:content-type "text/html")
-		   (,(cl-who:with-html-output-to-string (s)
-							(:htm (:head)
-							      (:body (:P "Error: could-not-find character"))))))))))
-	((and
-	  (property :path-info (ppcre "^/character/(\\d*)$" id))
-	  (property :remote-user username))
-	 (with-db-connection
-	   (if (user-can-edit-character-p id username)
-	       (let ((character (get-character id)))
-		 (if character
-		     `(200
-		       (:content-type "text/html")
-		       (,(render-page
-			  (s)
-			  (:script
-			   (format s "React.render(React.createElement(CharacterMenu, {characterId: ~D, defaultValue : " id)
-			   (princ "fixupFcCharacter(Immutable.fromJS(" s)
-			   (encode-classish character s)
-			   (princ "))}),document.getElementById(\"react-content\"))" s)))))
-		     `(404
-		       (:content-type "text/html")
-		       (,(cl-who:with-html-output-to-string (s)
-							    (:htm (:head)
-								  (:body (:P "Error: could-not-find character"))))))))
-	       `(303
-		 (:location ,(fixup-path (format nil "/view-character/~d" id)))
-		 (())))))
-	((and
-	  (property :path-info "/logout/")
-	  (property :session session))
-	 (invalidate-session session)
-	 `(303
-	   (:location ,(fixup-path "/"))
-	   (())))
-	((and
-	  (property :path-info "/account/")
-	  (property :remote-user username))
-	 (with-db-connection
-	   `(200
-	     (:content-type "text/html")
-	     (,(render-page
-		(s)
-		(:script
-		 (format s
-			 "React.render(React.createElement(AccountSettings, {user: \"~A\"}),document.getElementById(\"react-content\"))" username)))))))
-	((and
-	  (property :path-info "/set-password/")
-	  (property :request-method :post)
-	  (property :remote-user username))
-	 (change-user-password username
-			       (flexi-streams:octets-to-string body :external-format :utf-8))
-	 `(200
-	   (:content-type "text/html")))
-	(_
-	 `(404
-	   (:content-type "text/html")
-	   (,(cl-who:with-html-output-to-string (s)
-						(:htm (:head) (:body (:p "Not found")))))))))))
+      (expand-app-rules env body
+			`(404
+			  (:content-type "text/html")
+			  (,(cl-who:with-html-output-to-string (s)
+							       (:htm (:head) (:body (:p "Not found"))))))))))
 
 
 (defun authenticate (user pass)
