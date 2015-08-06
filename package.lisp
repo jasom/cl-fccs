29abac82fc55a1b70aff9c8a881bc21ae8812db7
Jason Miller <aidenn0@geocities.com>
2015-08-05 23:13:40 -0700

    Fixed to work with upstream parenscript
    Upstream modified my patch to only emit .call on implicit lambdas if
    ps:this is used in the implicit lambda.  This is fine, but I was not
    previously importing ps:this, so it broke my code.
    
    This commit fixes it.

--- package.lisp
+++ package.lisp
@@ -3,7 +3,7 @@
 (defpackage #:cl-fccs
   (:use #:cl #:alexandria #:parenscriptx #:split-sequence
 	#:cl-who #:optima #:optima.ppcre #:parse-number)
-  (:import-from :parenscript :chain :create :getprop))
+  (:import-from :parenscript :chain :create :getprop :this))
 
 (defpackage #:fccg (:use #:cl))
 
