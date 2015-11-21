adedc424040d3d7993280681e3d6a2037d0a0847
Jason Miller <aidenn0@geocities.com>
2015-11-20 21:23:36 -0800

    Fix typo in closure compiler jar filename

--- build/config.sh
+++ build/config.sh
@@ -1,6 +1,6 @@
 PATH=$PATH:/opt/local/bin
 JSX="$HOME/gh/react/bin/jsx"
-CLOSURE="$HOME/src/clojure-compiler.jar"
+CLOSURE="$HOME/src/closure-compiler.jar"
 OPT=DEBUG
 REACTJS="$HOME/gh/react/build/react-with-addons.js"
 IMMUTABLEJS="$HOME/gh/immutable-js/dist/immutable.min.js"
