00c9bd34e9ae9b79fd5c3978ef0c73c98db36243
Jason Miller <aidenn0@geocities.com>
2016-01-22 20:38:04 -0800

    Simplify default configuration
    This should allow building without external dependencies

--- build/all.css.do
+++ build/all.css.do
@@ -4,4 +4,4 @@ SOURCES="$SRCROOT/pure-min.css
 $SRCROOT/grid.css
 $SRCROOT/local.css"
 redo-ifchange $SOURCES ./config.sh
-cat $SOURCES |java -jar "$YUICOMP" --type css
+cat $SOURCES |if test -z "$YUICOMP"; then cat; else java -jar "$YUICOMP" --type css; fi
--- build/config.sh
+++ build/config.sh
@@ -1,8 +1,8 @@
 PATH=$PATH:/opt/local/bin
-JSX="$HOME/gh/react/bin/jsx"
 CLOSURE="$HOME/src/closure-compiler.jar"
 OPT=DEBUG
-REACTJS="$HOME/gh/react/build/react-with-addons.js"
-IMMUTABLEJS="$HOME/gh/immutable-js/dist/immutable.min.js"
-YUICOMP="$HOME/src/yuicompressor-2.4.8.jar"
-PRETTY="$HOME/psx/node_modules/js-beautify/js/bin/js-beautify.js -f -"
+REACTJS="static/js/react-with-addons.js"
+IMMUTABLEJS="static/js/immutable.min.js"
+YUICOMP=""
+#PRETTY="$HOME/psx/node_modules/js-beautify/js/bin/js-beautify.js -f -"
+PRETTY=cat
